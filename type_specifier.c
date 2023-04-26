c63fe4b06716a582597e61196b338a44707077c3
Pie Pie <i.jalal@aui.ma>
2023-04-26 07:11:37 +0000

    Update type_specifier.c

--- type_specifier.c
+++ type_specifier.c
@@ -100,14 +100,44 @@ int dec_setter(va_list valist)
 */
 int int_setter(va_list valist)
 {
-	char *p_buff;
-	int size;
+	int len, powten, j, digit, n, count = 0, num;
 
-	p_buff = itoa(va_arg(valist, int), 10);
-
-	size = print((p_buff != NULL) ? p_buff : "NULL");
-
-	return (size);
+	n = va_arg(i, int);
+	if (n != 0)
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			count++;
+		}
+		num = n;
+		len = 0;
+		while (num != 0)
+		{
+			num /= 10;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 10;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			if (n < 0)
+				_putchar((digit * -1) + 48);
+			else
+				_putchar(digit + '0');
+			count++;
+			n -= digit * powten;
+			powten /= 10;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
 }
 /**
 * bin_setter - convert to binary
