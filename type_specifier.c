b3b51df7229c7663c41d0052c14bde17f65864d1
Pie Pie <i.jalal@aui.ma>
2023-04-26 07:09:04 +0000

    Update type_specifier.c

--- type_specifier.c
+++ type_specifier.c
@@ -133,14 +133,37 @@ int bin_setter(va_list valist)
  **/
 int unsig_setter(va_list valist)
 {
-	char *p_buff;
-	int size;
+	unsigned int len, powten, j, digit, n, num;
+	int count = 0;
 
-	p_buff = itoa(va_arg(valist, unsigned int), 10);
-
-	size = print((p_buff != NULL) ? p_buff : "NULL");
-
-	return (size);
+	n = va_arg(valist, unsigned int);
+	if (n != 0)
+	{
+		num = n;
+		len = 0;
+		while (num != 0)
+		{
+			num /= 10;
+			len++;
+		}
+		powten = 1;
+		for (j = 1; j <= len - 1; j++)
+			powten *= 10;
+		for (j = 1; j <= len; j++)
+		{
+			digit = n / powten;
+			_putchar(digit + '0');
+			count++;
+			n -= digit * powten;
+			powten /= 10;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
 }
 /**
  * octal_setter - Print a unsigned octal
